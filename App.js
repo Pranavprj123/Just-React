815c9b1746960097f05d89033b39959a65467e8b
Pranavprj <jadhavpranav64@gmail.com>
2025-08-17 12:37:51 +0530

    first react

---
+++ App.js
@@ -0,0 +1,18 @@
+const parent = React.createElement("div", {id: "parent"},
+    React.createElement("div", {id: "child1"}, 
+    [React.createElement("h1",{},"This is h1 child element"),
+     React.createElement("h2",{},"This is h2 child element")
+    ]),
+    React.createElement("div", {id: "child2"}, 
+    [React.createElement("h1",{},"This is h1 child element"),
+     React.createElement("h2",{},"This is h2 child element")
+    ]),
+    
+ );
+
+ console.log(parent);
+
+
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
---
+++ README.md
@@ -0,0 +1,1 @@
+This is my First Step into React 🚀
