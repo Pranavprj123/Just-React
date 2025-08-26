7c01d8b5fd385b04dae7e88d4d6db2c4db19108e
Pranavprj <jadhavpranav64@gmail.com>
2025-08-26 14:18:42 +0530

    functional components

--- App.js
+++ App.js
@@ -4,10 +4,26 @@ import ReactDOM from "react-dom/client";
 
 
 //JSX - it is not HTML in JS        it is HTMl like Syntax or XML Syntax
-const jsxHeading = <h1 id="heading">Hello World from JSX!</h1>;
 
-console.log(jsxHeading);
+
+const title =  (
+    <h1 className="head" tabIndex={1}>
+        Hello World from JSX!
+    </h1>
+);
+
+//React Functional Component
+
+//Component Composition
+const HeadingComponent = () => (
+    <div id="container">
+        {/* Title is the Component Composition */}
+        {title}
+        <h1 className="header">Hello World from Functional Component!</h1>
+    </div>
+);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
